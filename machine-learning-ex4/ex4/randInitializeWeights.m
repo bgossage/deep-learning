ce5d95c91b719a43a4bba88b25a5e8ee7d44533b
Brett Gossage <bgossage3@comcast.net>
2018-02-04 15:42:58 -0600

    implemented

--- machine-learning-ex4/ex4/randInitializeWeights.m
+++ machine-learning-ex4/ex4/randInitializeWeights.m
@@ -18,13 +18,11 @@ W = zeros(L_out, 1 + L_in);
 %
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
+%epsilon = sqrt(6.0) / sqrt(L_in + L_out);
 
-
-
-
-
-
-
+% Randomly initialize the weights to small values
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2.0 * epsilon_init - epsilon_init;
 
 
 % =========================================================================
